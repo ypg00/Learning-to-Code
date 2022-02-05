455c9215918a7e181b348859d5a23db63ef2a545
ypg00 <98235230+ypg00@users.noreply.github.com>
2022-02-05 18:07:15 -0500

    buggy cmd line arg inputs

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
-    printf("hello world!\n");
+    printf("hello world! - %d\n", argc);
+    for (int i = 0; i < argc; i++)
+    {
+        printf("arg %d - %s, %i, %f\n", i, argv[i], atoi(argv[i]), atof(argv[i]));
+    }
 }
